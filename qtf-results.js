99f3f34528fb4da31b6ad494f4a7cd58609ca8a8
miko <you@example.com>
2022-01-15 23:04:01 +0200

    Minor improvements to the most basic web page

--- qtf-results.js
+++ qtf-results.js
@@ -9,19 +9,21 @@ function toggleNextRow(row) {
 }
 
 function main() {
-	let body = document.querySelector("body");
+	let main_div = document.querySelector("#main_div");
 
 	let main_table = document.createElement("table");
+	main_table.id = "results_table";
 	let header_texts = [
 		"Proposal URL",
-		"Contributors",
 		"Channel",
+		"Contributors",
 		"Contributed",
 		"Matched",
 	];
 
 	//Create headers
 	let tr = document.createElement("tr");
+	tr.classList.add("header_row");
 	header_texts.forEach((item) => {
 		let th = document.createElement("th");
 		th.innerHTML = item;
@@ -35,8 +37,8 @@ function main() {
 		tr.classList.add("proposal-row");
 		tr.innerHTML = `
 		<td><a href="${getOdyseeURL(proposal.claim.permanent_url)}">${proposal.claim.value.title}</a></td>
-		<td>${proposal.contributors.length}</td>
 		<td><a href="${getOdyseeURL(proposal.claim.signing_channel.permanent_url)}">${proposal.claim.signing_channel.name}</a></td>
+		<td>${proposal.contributors.length}</td>
 		<td>${proposal.accepted_amount.toFixed(2)} (${proposal.funded_amount.toFixed(2)}) LBC</td>
 		<td>${proposal.matched_amount.toFixed(2)} LBC</td>
 		`;
@@ -78,7 +80,7 @@ function main() {
 		});
 
 		let tr3 = document.createElement("tr");
-		tr3.innerHTML = `<th colspan="100%">Invalid supports</th>`;
+		tr3.innerHTML = `<td colspan="100%"; style="text-align:center">Invalid supports</td>`;
 		tr3.classList.add("invalid-supports-title-row");
 		tr3.onclick = () => toggleNextRow(tr3);
 		contributions_table.append(tr3);
@@ -112,31 +114,69 @@ function main() {
 		
 
 	});
-	body.append(main_table);
+	main_div.append(main_table);
 
 
-
-	//Fill list with round details
-	let ul = document.querySelector("ul");
+	//Make round details table
+	
+	let round_details_table = document.createElement("table");
+	round_details_table.id = "round_details_table";
+	tr = document.createElement("tr");
+	tr.innerHTML = `
+		<th colspan="100%">Round details</th>
+	`;
+	round_details_table.append(tr);
 	for (const [key, value] of Object.entries(qtf_results)) {
 		if ( typeof(value) != 'object'  ) {
 			let text = key.replaceAll("_", " ");
 			text = text[0].toUpperCase() + text.substring(1);
-			let li = document.createElement("li");
-			li.innerHTML = `${text}: ${value}`;
-			ul.append(li);
+			let tr = document.createElement("tr");
+			tr.innerHTML = `
+				<td>${text}</td>
+				<td>${value}</td>
+			`;
+			round_details_table.append(tr);
 		  console.log(`${text}: ${value}`);
 		} else if (key == "round_details") {
 			for (const [kez, value] of Object.entries(qtf_results[key])) {
 			let text = kez.replaceAll("_", " ");
 			text = text[0].toUpperCase() + text.substring(1);
-			let li = document.createElement("li");
-			li.innerHTML = `${text}: ${value}`;
-			ul.append(li);
+			let tr = document.createElement("tr");
+			tr.innerHTML = `
+				<td>${text}</td>
+				<td>${value}</td>
+			`;
+			round_details_table.append(tr);
 		  console.log(`${text}: ${value}`);
 			}
 		}
 	}
+	let hr = document.createElement("hr");
+	main_div.append(hr);
+	main_div.append(round_details_table);
+
+
+	//Fill list with round details
+//	let ul = document.querySelector("ul");
+//	for (const [key, value] of Object.entries(qtf_results)) {
+//		if ( typeof(value) != 'object'  ) {
+//			let text = key.replaceAll("_", " ");
+//			text = text[0].toUpperCase() + text.substring(1);
+//			let li = document.createElement("li");
+//			li.innerHTML = `${text}: ${value}`;
+//			ul.append(li);
+//		  console.log(`${text}: ${value}`);
+//		} else if (key == "round_details") {
+//			for (const [kez, value] of Object.entries(qtf_results[key])) {
+//			let text = kez.replaceAll("_", " ");
+//			text = text[0].toUpperCase() + text.substring(1);
+//			let li = document.createElement("li");
+//			li.innerHTML = `${text}: ${value}`;
+//			ul.append(li);
+//		  console.log(`${text}: ${value}`);
+//			}
+//		}
+//	}
 
 }
 
